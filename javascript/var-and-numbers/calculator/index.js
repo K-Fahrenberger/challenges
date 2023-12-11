94d3931d4ea3f0d79ca2711f2e37794e92b4ca9f
Kevin Fahrenberger <fahrenberger.k@web.de>
2023-12-11 13:39:12 +0100

    add tip-calculator-solution

---
+++ javascript/var-and-numbers/calculator/index.js
@@ -0,0 +1,13 @@
+let mealCost = 2;
+let tipPercentage = 0.1;
+// Calculate the tip amount by multiplying the meal cost by the tip percentage, and store the result in a variable called tipAmount
+
+tipAmount = mealCost * tipPercentage;
+
+// Calculate the total cost of the meal, including tip, by adding the mealCost variable and the tipAmount variable, and store the result in a variable called totalCost.
+
+totalCost = mealCost + tipAmount;
+
+// Log out a message using the console.log() method that should look like this: // The total cost of your bill is: 33 euros
+
+console.log("The total cost of your bill is: " + totalCost + "euros");
