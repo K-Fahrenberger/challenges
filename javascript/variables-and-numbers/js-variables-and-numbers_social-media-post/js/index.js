0904f51d79ff2a14b892c0f904a26908d1020bb3
Kevin Fahrenberger <fahrenberger.k@web.de>
2023-12-11 13:15:40 +0100

    add data-social-media-solution

---
+++ javascript/variables-and-numbers/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const title = "I like title";
+const text = "I like text";
+let theNumberOfLikes = 100;
+const UserWhoCreatedThePost = "Jeff";
+let isReported = false;
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log("Title:", title);
+console.log("Text:", text);
+console.log("Number of Likes:", numberOfLikes);
+console.log("User who created the post:", userWhoCreatedThePost);
+console.log("Is Reported:", isReported);
+
+numberOfLikes++;
+
+console.log("Updated Number of Likes:", numberOfLikes);
+// --^-- write your code here --^--
