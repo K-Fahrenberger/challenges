bebc26868a434323feb0d3dcc506452d81d55df0
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-02-01 22:33:42 +0100

    add: props_sum solution

--- react/props/react-props_sum/src/App.js
+++ react/props/react-props_sum/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return <h1>Replace me with your component!</h1>;
-}
+import React from 'react';
+
+const Sum = ({ valueA, valueB }) => {
+  const result = valueA + valueB;
+  return <p>{valueA} + {valueB} = {result}</p>;
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sum valueA={5} valueB={7} />
+    </div>
+  );
+};
+
+export default App
